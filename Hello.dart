c1f07ac34a7248827972080c71897d1288e51823
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-12-31 13:11:25 +0600

    dart exercise number round

--- Hello.dart
+++ Hello.dart
@@ -1,3 +1,31 @@
+import 'dart:math';
+
+void dsType() {
+  var myName = "imuk";
+  print(myName.length);
+  print(myName.toUpperCase());
+
+  var stringNum = "20";
+  print(stringNum.runtimeType);
+
+  var convertedNum = double.parse(stringNum);
+  print(convertedNum.runtimeType);
+
+  var someNum = 3.1;
+  print(someNum.floor());
+
+  var underNum = 0.5;
+  print(underNum.floor());
+}
+
 void main() {
-  print("Hello World");
+  print(1 + 1);
+  print(1 % 1);
+  print(1 / 1);
+  print(3 * 5);
+  print(10 / 0);
+//   avasd
+//     asdsadsa
+  print(pow(2, 4));
+  dsType();
 }
